6b78955d729de70d33833d795a2f38fbc236df21
Petr Severa <segedacz@gmail.com>
2017-04-12 22:13:30 +0200

    unlicense in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject czech-holidays "0.1.0"
   :description "Library containing Czech holidays"
   :url "https://github.com/segeda/czech-holidays-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The Unlicense"
+            :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.13.0"]])
